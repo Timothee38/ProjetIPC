65806d793702471acad2f1c8ba467404696cdcd8
Timothee38 <timothee.craig@gmail.com>
2016-12-21 13:19:24 +0100

    Fix some stuff for alex :)

--- IPC/agence.c
+++ IPC/agence.c
@@ -8,7 +8,6 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
-
 #include "shmem.h"
 #include "semaphore.h"
 #include "sharedMemory.h"
@@ -22,6 +21,31 @@ void fin(){
 	exit(1);
 }
 
+void verificationMemoire(SHMEM* ShrdMem, char *commande, int seats){
+	printf("Entree Verif");
+	int i;
+	int j;
+	int ok = 0;
+
+  for(i = 0; i < 20; i++) {
+	  ok = 0;
+	  for(j = 0; j < 20; j++){
+		if(ShrdMem[i].destination[j] == commande[j]){
+			ok = ok+1;
+		 }
+	  }
+   }
+   if(ok == 20){
+		if(ShrdMem[i].nbSeats >= seats){
+		ShrdMem[i].nbSeats = ShrdMem[i].nbSeats - seats;
+		printf("Commande Valide!\n");
+		}
+	}else{
+		printf("Rien de valide!\n");
+	}
+}
+
+
 void commandeValide(){
 	//Ne fait rien pour le moment
 }
@@ -31,14 +55,23 @@ void commandeInvalide(){
 }
 
 void main(){
-	int pid;
-	key_t cleBoite = 300;
-	int idBoite;
 
 	struct msgbuf{
 		long messageType;
 		char mess[20]; //20 ou 21?
 	} message, commande;
+
+	int cleMutex = 100;
+	int cleShMem = 200;
+	key_t cleBoite = 300;
+
+	int pid;
+	int idBoite;
+	int idMutex;
+
+	idMutex = open_semaphore(cleMutex);
+	printf("Semaphore Mutex Ouvert\n");
+
 
 	pid = getpid();
 
@@ -63,29 +96,59 @@ void main(){
 
 		signal(SIGUSR1, commandeValide); //Protection des signaux de validation de l'agence
 		signal(SIGUSR2, commandeInvalide);
-		char comm[20] = "";
+		char comm = "";
 		while(1){
 
 			scanf("%s", &comm);
 
 			commande.messageType = 1;
-			sprintf(commande.mess,"%s",comm);
+			sprintf(commande.mess,"%s",&comm);
 			msgsnd(idBoite, &commande, sizeof(commande.mess),0);
 			printf("Message Envoyé!\n");
 			}
 
 	default : //Le process Agence lui même
 		printf("Bienvenue chez l'agence de Voyage, les derniers vols sont actualisés régulièrement!\n");
-		while(1){
+		int i;
+		int seats = 0;
+		SHMEM *ShrdMem = (SHMEM *)attach_shmem(open_shmem(cleShMem,sizeof(SHMEM)*20));
+		char destRecue[21];
+		int selection = 0;
+
+		printf("Mémoire connectée!\n");
+		while(selection == 0){
+
+			destRecue[21] = "????????????????????\0";
+
+
+
 
 			msgrcv(idBoite, &message, cleBoite, 1,0);
 			printf("Message recu\n");
 			sleep(1);
 			printf("Commande Recue : %s\n", message.mess);
+			printf("longeur chaine recue : %d\n", strlen(message.mess));
+
+			if(strlen(message.mess) > 2){
+			for(i=0; i < strlen(message.mess); i++){
+				destRecue[i] = message.mess[i];
+			}
+			printf("Destination Recue : %s\n", destRecue);
+			}else{
+				seats = atoi(message.mess);
+				printf("Commande Recue : %d\n", seats);
+				selection = 1;
+
+			}
 
 			//Verifications a faire dans la mémoire partagée et renvoyer le signal correspondant
 
 		}
+		printf("Entree mutex\n");
+			down(idMutex);
+
+			verificationMemoire(ShrdMem, destRecue,seats);
+			up(idMutex);
 	}
 
 }
